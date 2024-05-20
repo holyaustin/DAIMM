6ca5bf40a090895cb07512f418ff42570a4e0828
holyaustin <holyaustin@yahoo.com>
2024-05-20 02:30:01 +0100

    header Commit

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -40,7 +40,7 @@ export const HeaderMenuLinks = () => {
               passHref
               className={`${
                 isActive ? "bg-secondary shadow-md" : ""
-              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-2xl  rounded-full gap-2 grid grid-flow-col`}
             >
               {icon}
               <span>{label}</span>
@@ -88,8 +88,8 @@ export const Header = () => {
             </ul>
           )}
         </div>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
+        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-14 mr-20 shrink-0">
+          <div className="flex relative w-14 h-14 mr-6">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
@@ -97,7 +97,7 @@ export const Header = () => {
             <span className="text-lg">Create, Sell & Buy A. I. Models</span>
           </div>
         </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
+        <ul className=" hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
           <HeaderMenuLinks />
         </ul>
       </div>
