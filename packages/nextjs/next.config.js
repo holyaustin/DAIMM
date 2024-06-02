2dd5f4a7a8a7852ab549f11c8971cb61a793be8f
holyaustin <holyaustin@yahoo.com>
2024-06-02 19:04:13 +0100

    fleek

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -2,13 +2,15 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "export",
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
+
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
